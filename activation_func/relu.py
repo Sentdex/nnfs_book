8d75e9d668301f59ac61b081172d414ba1362237
Petar Ceho <petarceho@gmail.com>
2023-04-03 22:12:23 +0200

    relu

---
+++ activation_func/relu.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+# ReLU activation
+class Activation_ReLU:
+
+    # Forward pass
+    def forward(self, inputs, training):
+        # Remember input values
+        self.inputs = inputs
+        #y=x if x>=0 else y = 0
+        self.output = np.maximum(0, inputs)
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Since we need to modify original variable,
+        # let's make a copy of values first
+        self.dinputs = dvalues.copy()
+
+        #d.out.grad/drelu = 1 if (x>=1) else 0
+        #slope is zero when x<0 since its a straight line
+        self.dinputs[self.inputs <= 0] = 0
+
+    # Calculate predictions for outputs
+    def predictions(self, outputs):
+        return outputs
