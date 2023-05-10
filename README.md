a1b9ffd50be3bf297ac32e2d7d5116ffcbcbdf9f
Petar Ceho <petarceho@gmail.com>
2023-05-10 13:52:24 +0200

    sgd with momentum

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Neural Networks from Scratch (NNFS) book code
 
-Code from the NNFS book (https://nnfs.io) separated by chapter. The NNFS book is written with the Python programming lanauge. If you're interested in seeing this same content, but with 40+ other programming languages, check out: https://github.com/sentdex/nnfsix
-
-
 Idea of the branch is to restructure the classes into packages, and provide training examples(MNIST etc..),and to add a batch-normalization layer to it,probably to try to implement convolution layer,pooling layer too to build a CNN.
 
  
--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -1,12 +1,46 @@
-s
+import numpy as np
 
 class Optimizer_SGD:
 
     #default learning rate is 0.1
-    def __init__(self,learning_rate=0.01):
+    def __init__(self,learning_rate=0.01,decay=0.,momentum=0.):
         self.learning_rate=learning_rate
+        self.current_learning_rate=learning_rate
+        self.decay=decay
+        self.iterations=0
+        self.momentum=momentum
 
+    #call once before update params,learning rate decay
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate=self.learning_rate*(1. /(1.+self.decay*self.iterations)) 
+
+    #update params
     def update_params(self,layer):
-        layer.weights+=-self.learning_rate*layer.dweights
-        layer.biases+=-self.learning_rate*layer.biases
+        #with sgd momentum 
+        if self.momentum:
+
+            if not hasattr(layer,'weight_momentums'):
+                layer.weight_momentums=np.zeros_like(layer.weights)
+                layer.bias_momentums=np.zeros_like(layer.biases)
+            
+            #weights updates with momentum
+            weight_updates=self.momentum *layer.weight_momentums-self.current_learning_rate*layer.dweights
+            layer.weight_momentums=weight_updates
+
+            bias_updates=self.momentum*layer.bias_momentums-self.current_learning_rate*layer.biases
+            layer.bias_momentums=bias_updates
+
+        #without momentum regular SGD
+        else:
+            weight_updates=-self.current_learning_rate*layer.dweights
+            bias_updates=-self.current_learning_rate*layer.biases
+
+        #update w/b
+        layer.weights+=weight_updates
+        layer.biases+=bias_updates
+
+    #call once after update params
+    def post_update_params(self):
+        self.iterations+=1
 
